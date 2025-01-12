1676491784cf677cc01967243b3e9bb20eefdd6a
Lucas Laredo <luckrocaabh@gmail.com>
2025-01-12 20:45:53 -0300

    updating this shit

--- recsys/update_deployment.py
+++ recsys/update_deployment.py
@@ -6,12 +6,12 @@ with open('configmap.yaml', 'r') as f:
 playlist_path = configmap['data']['PLAYLIST_PATH']
 dataset_name = playlist_path.split('/')[-1].split('.')[0]
 
-with open('./deployment.yaml', 'r') as f:
+with open('deployment.yaml', 'r') as f:
     deployment = yaml.safe_load(f)
 
 deployment['metadata']['name'] = dataset_name
 
-with open('./deployment.yaml', 'w') as f:
+with open('deployment.yaml', 'w') as f:
     yaml.safe_dump(deployment, f)
 
 print(f"Updated deployment.yaml with metadata.name: {dataset_name}")
