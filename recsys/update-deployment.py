51a6676255aa68783d0d0cc7fa505105283946cc
Lucas Laredo <luckrocaabh@gmail.com>
2025-01-13 20:00:11 -0300

    adding pipeline script

---
+++ recsys/update-deployment.py
@@ -0,0 +1,19 @@
+import yaml
+
+with open('configmap.yaml', 'r') as f:
+    configmap = yaml.safe_load(f)
+
+playlist_path = configmap['data']['PLAYLIST_PATH']
+dataset_name = playlist_path.split('/')[-1].split('.')[0]
+
+with open('deployment.yaml', 'r') as f:
+    deployment = yaml.safe_load(f)
+
+dataset_name = dataset_name.replace('_', '-')
+
+deployment['metadata']['name'] = f"lucaslaredo-playlist-recommender-deployment-{dataset_name}"
+
+with open('deployment.yaml', 'w') as f:
+    yaml.safe_dump(deployment, f)
+
+print(f"Updated deployment.yaml with metadata.name: {dataset_name}")
